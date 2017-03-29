5b6deb57e3792f64a3797c11b0860c9bc8663e4a
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2017-03-29 15:25:57 +0300

    Forgot to add to last commit.

--- Minesweeper/Startup.cs
+++ Minesweeper/Startup.cs
@@ -7,14 +7,32 @@ public class Startup
         const int fieldDimension = 10;
         const int mineCount = 10;
 
-        Console.Clear();
-        PrintGreeting();
-        PrintRules();
-        Console.Clear();
+        StartView();
 
         Minefield m = new Minefield(fieldDimension, mineCount);
         m.PreviewMinefield(true);
         Console.ReadKey();
+    }
+
+    private static void StartView()
+    {
+        Console.Clear();
+        PrintGreeting();
+        Delay(2000);
+        PrintRules();
+        Delay(2000);
+        PrintControls();
+        Delay(2000);
+        Console.Clear();
+    }
+
+    private static void PrintControls()
+    {
+        Console.WriteLine("Input syntax:");
+        Delay(750);
+        Console.WriteLine($"open {{row}} {{col}} {Environment.NewLine}flag {{row}} {{col}}");
+        Delay(1500);
+        Console.WriteLine("Good Luck.");
     }
 
     private static void PrintGreeting()
@@ -25,12 +43,20 @@ public class Startup
     private static void PrintRules()
     {
         Console.WriteLine("The rules are simple:");
+        Delay(1000);
         Console.WriteLine("Kill or be killed.");
+        Delay(1000);
         Console.WriteLine("Understood?");
-        Console.WriteLine("y/n");
-        while (Console.ReadLine().Trim().ToLower() != "y") ;
-
+        if(Console.Read() != 'y')
+        {
+            Console.WriteLine("Too bad.");
+        }
     }
 
-
+    private static void Delay(int millis)
+    {
+        var startTime = DateTime.Now;
+        var endTime = startTime.AddMilliseconds(millis);
+        while (startTime < endTime) startTime = DateTime.Now;        
+    }
 }
